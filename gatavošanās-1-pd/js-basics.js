883408d0ddccdf4866a739bff317bcc63513ab0a
Lillija <asikeligalillija@gmail.com>
2024-11-20 14:56:05 +0200

    Add files via upload

---
+++ gatavošanās-1-pd/js-basics.js
@@ -0,0 +1,39 @@
+//variables
+
+// let firstName = "Jānis";
+// console.log(firstName);
+// let myArray = [1,2,3,4];
+
+
+//conditionals
+
+// let x = 10;
+// if (x == 10){
+//     console.log("x ir lielāks par 10");
+// }
+
+
+// let person = {
+//     vards: "Antra",
+//     irStudents: true,
+// }
+// // console.log (person.irStudents); //dot notation
+// // console.log (person["vards"]); //bracket notation
+
+
+// let attribute = "vards";
+// console.log(person[attribute])
+// person.vecums = 16
+// console.log(person);
+
+// //ternary operator
+// let ageStatus = (personai.vecums >=18) ? "Pilngadīgs" : "Nepilngadīgs";
+// console.log(ageStatus);
+
+// let namesArray = ["Antra", "Ilmārs", "Renārs", "Pauls"] 
+// //indeksi ir attiecīgi [0,1,2,3]
+// namesArray[2]; //Izvadīs vārdu Renārs
+
+// for (let i=0;i<namesArray.length;i++){
+//     console.log(namesArray[i]);
+// }
